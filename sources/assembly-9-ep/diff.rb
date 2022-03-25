369c258d9d3defc1f483047e6ee558dad57701f4
Tony Bowden <tony@tmtm.com>
2022-03-25 12:30:21 +0200

    Assembly 9: compare to EP

---
+++ sources/assembly-9-ep/diff.rb
@@ -0,0 +1,13 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/comparison'
+
+class Comparison < EveryPoliticianScraper::NulllessComparison
+  def external_csv_options
+    { converters: [->(val) { val == 'Q3366357' ? 'Q953447' : val }] }
+  end
+end
+
+diff = Comparison.new('wikidata.csv', 'scraped.csv').diff
+puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
