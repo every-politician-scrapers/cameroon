d5db56413fb3dc2c0b70436bb688854d7b2a72ba
Tony Bowden <tony@tmtm.com>
2022-03-29 14:11:30 +0300

    Compare 2nd Term senators
    There doesn't appear to be an official site, but the list was
    published at
      https://www.crtv.cm/2018/04/liste-des-complete-des-senateurs-de-la-legislature/
    
    (Thanks to Charlie Martial NGOUNOU for the pointer)

---
+++ sources/senate-10/create-missing-member.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+let rawmeta = fs.readFileSync('meta.json');
+let meta = JSON.parse(rawmeta);
+
+module.exports = (label,region,party) => {
+  mem = {
+    value: meta.position,
+    qualifiers: {
+      P2937: meta.term.id,
+      P768: region,
+    },
+    references: {
+      P4656: meta.source,
+      P813: new Date().toISOString().split('T')[0],
+      P1810: label,
+    }
+  }
+  if(party) mem['qualifiers']['P4100']  = party
+
+  claims = {
+    P31: { value: 'Q5' }, // human
+    P106: { value: 'Q82955' }, // politician
+    P39: mem,
+  }
+
+  return {
+    type: 'item',
+    labels: { en: label },
+    descriptions: { en: 'politician in Cameroon' },
+    claims: claims,
+  }
+}
